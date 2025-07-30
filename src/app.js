dfd2f9f1de403dffff886c75bb54b1013f318647
Ange-Mukundente <a.mukundent@alustudent.com>
2025-07-30 08:42:14 +0200

    feat: initial clean project setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// src/app.js  
+
+const express = require('express');
+const path = require('path'); 
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
+
+app.get('/', (req, res) => {
+    res.render('index', { message: "The Pipeline is Working!" });
+});
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000');
+});
