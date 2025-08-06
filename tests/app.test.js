c878a0a4bb47db10b21465d09165012a0b9bf277
Ange-Mukundente <a.mukundent@alustudent.com>
2025-08-06 09:57:59 +0200

    fix: update unit test to expect new feature text

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,9 +7,10 @@ const request = require('supertest');
 const app = require('../src/app'); 
 
 describe('App', () => {
-    it('should return a welcome message at the root', async () => {
-        const res = await request(app).get('/');
-        expect(res.statusCode).toEqual(200);
-        expect(res.text).toContain('The Pipeline is Working!');
-    });
+   it('should return a welcome message at the root', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    // This line now correctly looks for the NEW message
+    expect(res.text).toContain('This is the new feature!'); 
+});
 });
